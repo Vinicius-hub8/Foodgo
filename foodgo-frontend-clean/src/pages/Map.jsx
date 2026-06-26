de266f79ff4a0e6f35920b984c2ea89f30843c7d
Vinícius Amaral <viniciusamaraloliveira8@gmail.com>
2026-06-26 12:54:09 -0300

    Fix syntax for useJsApiLoader googleMapsApiKey

--- foodgo-frontend-clean/src/pages/Map.jsx
+++ foodgo-frontend-clean/src/pages/Map.jsx
@@ -126,8 +126,8 @@ export const Map = () => {
   const mapRef = useRef(null);
 
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-  });
+  googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+});
 
   // Carrega pontos do usuário
   useEffect(() => {
